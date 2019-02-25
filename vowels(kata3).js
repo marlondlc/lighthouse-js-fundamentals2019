5f355ef48feaec43b3b26e6080e2bfee6ff76b11
Marlon <m.de.la.cruz@hotmail.com>
2019-02-24 23:07:52 -0500

    clearer way of doing same thing

--- vowels(kata3).js
+++ vowels(kata3).js
@@ -14,3 +14,25 @@ console.log(numberOfVowels("orange"));
 console.log(numberOfVowels("lighthouse labs"));
 console.log(numberOfVowels("aeiou"));
 console.log(numberOfVowels("happy to see you made it"));
+
+
+//PREFER THE 2nd OPTION OF DOING THE SAME THING:
+
+/*
+let numberOfVowels = function(data) {
+  var vCounter = 0;
+  for (var i = 0; i < data.length; i++) {
+    if (data.charAt(i) === 'a' || 
+        data.charAt(i) === 'e' || 
+        data.charAt(i) === 'i' || 
+        data.charAt(i) === 'o' || 
+        data.charAt(i) === 'u') 
+        {vCounter++;
+    }
+  }
+  return vCounter;
+};
+
+console.log(numberOfVowels("orange"));
+console.log(numberOfVowels("lighthouse labs"));
+console.log(numberOfVowels("aeiou"));
